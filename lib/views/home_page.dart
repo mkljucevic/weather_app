a46396e795894ee6ecc15b44fdf4baab7fa8b21c
Mario Ključević <mkljucevic06dev@gmail.com>
2023-12-15 22:34:34 +0100

    feat: add elements and drawer opening functionality

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home page'),
+    return Scaffold(
+      key: scaffoldKey,
+      drawer: const Drawer(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => scaffoldKey.currentState?.openDrawer(),
+                    child: const Icon(Icons.menu),
+                  ),
+                  const SizedBox(width: 20),
+                  const Text('Tuesday 25, July'),
+                  const Spacer(),
+                  const Icon(Icons.calendar_month),
+                  const SizedBox(width: 20),
+                  const Icon(Icons.settings),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
