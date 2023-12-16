cb98190c60272b5b1387199f2da29c57e0c67e50
Mario Ključević <mkljucevic06dev@gmail.com>
2023-12-16 09:07:02 +0100

    feat: add sunrise and sunset card

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:weather_app/components/hourly_forecast_card.dart';
 import 'package:weather_app/components/main_weather_display_card.dart';
 
@@ -69,6 +70,38 @@ class _HomePageState extends State<HomePage> {
                       child: HourlyForecastCard(),
                     );
                   },
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    const Text('Sunrise'),
+                    const SizedBox(width: 15),
+                    SvgPicture.asset(
+                      'assets/sun.svg',
+                      width: 20,
+                      height: 20,
+                    ),
+                    const SizedBox(width: 5),
+                    const Text('04:54'),
+                    const Spacer(),
+                    const Text('Sunset'),
+                    const SizedBox(width: 15),
+                    SvgPicture.asset(
+                      'assets/moon.svg',
+                      width: 20,
+                      height: 20,
+                    ),
+                    const SizedBox(width: 5),
+                    const Text('21:06'),
+                  ],
                 ),
               ),
             ],
