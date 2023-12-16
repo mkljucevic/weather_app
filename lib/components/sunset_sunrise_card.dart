2149010d1c0e8ddb9897ff35bfb579bf96485050
Mario Ključević <mkljucevic06dev@gmail.com>
2023-12-16 09:31:50 +0100

    refactor: paddings and widgets

---
+++ lib/components/sunset_sunrise_card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SunriseSunsetCard extends StatelessWidget {
+  const SunriseSunsetCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: Row(
+        children: [
+          const Text('Sunrise'),
+          const SizedBox(width: 15),
+          SvgPicture.asset(
+            'assets/sun.svg',
+            width: 20,
+            height: 20,
+          ),
+          const SizedBox(width: 5),
+          const Text('04:54'),
+          const Spacer(),
+          const Text('Sunset'),
+          const SizedBox(width: 15),
+          SvgPicture.asset(
+            'assets/moon.svg',
+            width: 20,
+            height: 20,
+          ),
+          const SizedBox(width: 5),
+          const Text('21:06'),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:weather_app/components/hourly_forecast_card.dart';
 import 'package:weather_app/components/main_weather_display_card.dart';
+import 'package:weather_app/components/sunset_sunrise_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -21,11 +22,12 @@ class _HomePageState extends State<HomePage> {
       key: scaffoldKey,
       drawer: const Drawer(),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              Row(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
+              child: Row(
                 children: [
                   GestureDetector(
                     onTap: () => scaffoldKey.currentState?.openDrawer(),
@@ -39,73 +41,50 @@ class _HomePageState extends State<HomePage> {
                   const Icon(Icons.settings),
                 ],
               ),
-              const SizedBox(height: 20),
-              Container(
-                  height: 170,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: PageView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 3,
-                    itemBuilder: (context, index) {
-                      return const Padding(
-                        padding: EdgeInsets.only(right: 10),
-                        child: MainWeatherDisplayCard(),
-                      );
-                    },
-                  )),
-              const SizedBox(height: 20),
-              Container(
-                height: 110,
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 5.0),
+              child: Container(
+                height: 170,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                 ),
-                child: ListView.builder(
+                child: PageView.builder(
                   scrollDirection: Axis.horizontal,
-                  itemCount: 10,
+                  itemCount: 3,
                   itemBuilder: (context, index) {
                     return const Padding(
                       padding: EdgeInsets.only(right: 10),
-                      child: HourlyForecastCard(),
+                      child: MainWeatherDisplayCard(),
                     );
                   },
                 ),
               ),
-              const SizedBox(height: 20),
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                ),
-                child: Row(
-                  children: [
-                    const Text('Sunrise'),
-                    const SizedBox(width: 15),
-                    SvgPicture.asset(
-                      'assets/sun.svg',
-                      width: 20,
-                      height: 20,
-                    ),
-                    const SizedBox(width: 5),
-                    const Text('04:54'),
-                    const Spacer(),
-                    const Text('Sunset'),
-                    const SizedBox(width: 15),
-                    SvgPicture.asset(
-                      'assets/moon.svg',
-                      width: 20,
-                      height: 20,
-                    ),
-                    const SizedBox(width: 5),
-                    const Text('21:06'),
-                  ],
-                ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 110,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
               ),
-            ],
-          ),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.only(left: 15),
+                    child: HourlyForecastCard(),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: SunriseSunsetCard(),
+            ),
+          ],
         ),
       ),
     );
