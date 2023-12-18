c159b724b0a34fdccc052ca16b171048f180b244
Mario Ključević <mkljucevic06dev@gmail.com>
2023-12-18 11:33:57 +0100

    feat: add app colors

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-// light theme colors
-const Color cBgColorLight = Color(0xfff3f3f3);
+// ========= universal colors =========
+const Color cYellow = Color(0xffffce49); // yellow color for sun icons
+const Color cYellow1 = Color(0xfff3e5ab); // light yellow for snow
+const Color cBlue1 = Color(0xff7db0f1); // lightest blue color
+const Color cBlue2 = Color(0xff4d7db4); // medium blue color
+const Color cBlue3 = Color(0xff213f61); // darkest blue color
 
-// dark theme colors
+// ========= light theme colors =========
+const Color cBgColorLight = Color(0xfff3f3f3); // light background color
+const Color cTextColorLight =
+    Color(0xff181818); // text color on light background
+const Color cCardBgLight =
+    Color(0xffffffff); // card background color on light background
+
+// ========= dark theme colors =========
 const Color cBgColorDark = Color(0xff272727);
+const Color cTextColorDark = Color(0xfff3f3f3); // text color on dark background
+const Color cCardBgDark =
+    Color(0xff363636); // card background color on dark background
